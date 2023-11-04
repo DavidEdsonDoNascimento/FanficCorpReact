5554c096c22edf2ff023bae8e4d433f860cc8eb0
David Edson do Nascimento <david.contato.tec@gmail.com>
2023-11-03 23:19:50 -0300

    FEAT: create aside and main with posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { Post, Header } from "./components";
+import { Post, Header, Sidebar } from "./components";
+import styles from "./App.module.css";
 import "./global.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <Post author="John" content="Hello" />
-      <Post author="John" content="Hello" />
-      <Post author="John" content="Hello" />
+      <div className={styles.wrapper}>
+       <Sidebar />
+        <main>
+          <Post 
+            author="Elena"
+            content="Fui promovida a ser parte da familia da empresa no meu primeiro dia, pra quem trabalha em uma empresa como essa nunca fica cansada."
+          />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.css";
+
+export const Sidebar = () => {
+  return <aside className={styles.sidebar}>o</aside>;
+};
