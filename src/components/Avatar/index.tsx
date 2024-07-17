942943f7d765c4a84b4d8ee185de3bdb03e6e96d
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-07-17 16:04:36 -0300

    Avatar component extends ImgHTMLAttributes

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.css';
+import { ImgHTMLAttributes } from 'react';
 
-interface IAuthorProps {
-	src: string;
+interface IAuthorProps extends ImgHTMLAttributes<HTMLImageElement> {
 	hasBorder?: boolean;
 }
 
